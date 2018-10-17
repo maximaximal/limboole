b96ea9a30308fb8df0c6e0fe54e6cda372e1f186
Max Heisinger <mail@maximaximal.com>
2018-10-17 14:34:32 +0200

    Also build windows 32 bit packages.

--- create_packages.sh
+++ create_packages.sh
@@ -7,18 +7,26 @@ chmod +x dockcross-linux-x64
 ./dockcross-linux-x64 cmake -Bbuild-linux-x64 -H. -DCMAKE_BUILD_TYPE=Release
 ./dockcross-linux-x64 make -C build-linux-x64 package
 
-echo "Create Windows Package."
+echo "Create Windows x64 Package."
 
 docker run --rm dockcross/windows-x64 > ./dockcross-windows-x64
 chmod +x dockcross-windows-x64
 ./dockcross-windows-x64 cmake -Bbuild-windows-x64 -H. -DCMAKE_BUILD_TYPE=Release -DUSE_LINGELING=Off
 ./dockcross-windows-x64 make -C build-windows-x64 package
 
+echo "Create Windows x32 Package."
+
+docker run --rm dockcross/windows-x86 > ./dockcross-windows-x32
+chmod +x dockcross-windows-x32
+./dockcross-windows-x32 cmake -Bbuild-windows-x32 -H. -DCMAKE_BUILD_TYPE=Release -DUSE_LINGELING=Off
+./dockcross-windows-x32 make -C build-windows-x32 package
+
 echo "Finished Creating Packages, copy to packages subfolder."
 mkdir -p packages
 
 cp ./build-linux-x64/packages/* ./packages
 cp ./build-windows-x64/packages/* ./packages
+cp ./build-windows-x32/packages/* ./packages
 
 echo "Finished Creating Packages! Created:";
 
