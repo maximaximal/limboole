8d207f764ef045777687816e3b032d9485fa2f3b
Max Heisinger <mail@maximaximal.com>
2020-09-28 12:52:34 +0200

    Fix wrong jquery invoke.

--- main.js
+++ main.js
@@ -184,8 +184,8 @@ function applyFromLocationHash() {
         selector.value = h.charAt(1);
         input.value = h.substring(2);
 
-        $(input).style.height = 'auto';
-        $(input).style.height = ($(input).scrollHeight) + 'px';
+        input.style.height = 'auto';
+        input.style.height = (input.scrollHeight) + 'px';
 
         window.LimbooleLoadedPromise.then(function() {
             input.onchange();
